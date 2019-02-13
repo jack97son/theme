e030068462b62c7d2e798ee36eaa8b5355c17c5c
Jackson <jackson.parada@globant.com>
2019-02-13 17:50:44 -0500

    theme nuevo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('mi primer tema');
+
+
+(function ($, Drupal, drupalSettings){
+
+    'use strict';
+
+    Drupal.behaviors.example = {
+        attach: function (context, settings) {
+            document.addEventListener('aos:in', ({ detail }) => {
+                console.log('animated in', detail);
+              });
+        }
+    };
+
+})(Jquery, Drupal, drupalSettings);
